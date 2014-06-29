ba29bc641d296761f214a468d5e5d6f57f049eae
Lucas Dixon <ldixon@google.com>
2014-06-29 15:37:33 -0400

    cleaned up promise definition; made thenable a class

--- third_party/promise/promise.d.ts
+++ third_party/promise/promise.d.ts
@@ -4,42 +4,35 @@
 // This file is part of the uProxy project, it's in this directory for ease of
 // reference and because we plan to move it to DefinitelyTyped.
 //
-// We may want to move to using:
-// https://github.com/borisyankov/DefinitelyTyped/blob/master/es6-promises/es6-promises.d.ts
+// TODO: Unlike the version in DefinitelyTypes, we would like to represent the
+// rejection error explicitly. The definitely Types version is here:
+//
+//   https://github.com/borisyankov/DefinitelyTyped/blob/master/es6-promises/es6-promises.d.ts
+//
+// We also provide some convenience functions that admit the type parameters and
+// use common defaults (void and Error).
 
-/**
- * Generic Errors that may have a stack attribute (as they do in JS).
- **/
+// Generic Errors that may have a stack attribute (as they do in JS).
 interface Error {
   stack?:any;  // TODO: fix `any`.
 }
 
-/**
- * Generic Thenable objects have a `then` type that can be `fulfilled` or
- * `rejected`.
- **/
-interface Thenable<T> {
-  then<T2>(fulfill:(t?:T) => Thenable<T2>, reject?:(e:Error) => Thenable<T2>)
-           : Thenable<T2>;
-  then<T2>(fulfill:(t?:T) => T2, reject?:(e:Error) => T2) : Thenable<T2>;
-  then(fulfill:(t?:T) => void, reject?:(e:Error) => void) : Thenable<void>;
+// Generic Thenable objects have a `then` type that can be `fulfilled` by an
+// object of type T or `rejected` with an error of type E.
+declare class Thenable<T> {
+  then<T2>(fulfill:(t?:T) => Thenable<T2>,
+           reject?:(e:Error) => Thenable<T2>) : Thenable<T2>;
+  then<T2>(fulfill:(t?:T) => T2,
+           reject?:(e:Error) => T2) : Thenable<T2>;
+  then(fulfill:(t?:T) => void,
+       reject?:(e:Error) => void) : Thenable<void>;
 }
 
-/**
- * Generic Promise for built-in js Promises. T is the `fullfillment object type`
- * type. The rejection object should always be javascript Error.
- */
-declare class Promise<T> {
+// Generic typing for built-in JS Promises. T is the `fullfillment object type`
+// type and E is the error type.
+declare class Promise<T> extends Thenable<T> {
   constructor(resolverFunction:(fulfill:(t?:T) => void,
                                 reject:(e:Error) => void) => void);
-
-  // then either returns subsiquent promise<T2> ...
-  then<T2>(fulfill:(t?:T) => Promise<T2>, reject?:(e:Error) => Promise<T2>)
-      : Promise<T2>;
-  // ... or the next fulfillment object directly ...
-  then<T2>(fulfill:(t?:T) => T2, reject?:(e:Error) => T2) : Promise<T2>;
-  // ... or full and error handlers return void.
-  then(fulfill:(t?:T) => void, reject?:(e:Error) => void) : Promise<void>;
 
   catch(catchFn:(e:Error) => Promise<T>) : Promise<T>;
   catch(catchFn:(e:Error) => T) : Promise<T>;
@@ -50,7 +43,6 @@ declare class Promise<T> {
   static resolve() : Promise<void>;
 
   static reject<T>(e:Error) : Promise<T>;
-  // Allow casting of Promise.reject to Promise<void>.
   static reject() : Promise<void>;
 
   static all<T>(promiseArray:Thenable<T>[]) : Promise<T[]>;
