159bf3f9eb7a6ca038e83e076cb0756bf2d38f6a
Trevor Johnston <trevj@google.com>
2014-07-16 15:15:20 -0400

    add webrtc typescript definitions

--- third_party/typings/tsd.d.ts
+++ third_party/typings/tsd.d.ts
@@ -1,2 +1,5 @@
 /// <reference path="es6-promise/es6-promise.d.ts" />
 /// <reference path="jasmine/jasmine.d.ts" />
+/// <reference path="webcrypto/WebCrypto.d.ts" />
+/// <reference path="webrtc/MediaStream.d.ts" />
+/// <reference path="webrtc/RTCPeerConnection.d.ts" />
