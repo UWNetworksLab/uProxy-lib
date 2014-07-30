a9657fd13fc4fee136daf07401656854fb3535aa
Lucas Dixon <ldixon@google.com>
2014-07-30 19:55:09 -0400

    separated common rules into tools dir

---
+++ tools/common-grunt-rules.ts
@@ -0,0 +1,93 @@
+//-----------------------------------------------------------------------------
+// Naming this `exports` is bit of a hack to allow this file to be compiled
+// normally and still used by commonjs-style require.
+module exports {
+  // Function to make a typescript rule based on expected directory layout.
+  export function typescriptSrc(name:string) {
+    return {
+      src: ['build/typescript-src/' + name + '/**/*.ts',
+            '!build/typescript-src/' + name + '/samples/**/*.ts',
+            '!build/typescript-src/' + name + '/**/*.d.ts'],
+      dest: 'build/',
+      options: {
+        basePath: 'build/typescript-src/',
+        ignoreError: false,
+        noImplicitAny: true,
+        sourceMap: true
+      }
+    };
+  }
+
+  // This is a typescript compilation rule that makes sure unit tests can
+  // typecheck with the declaration files only. This is a quick way to check
+  // declaration files are approximately valid/match the implementation file.
+  export function typescriptSpecDecl(name:string) {
+    return {
+      src: ['build/typescript-src/' + name + '/**/*.spec.ts',
+            'build/typescript-src/' + name + '/**/*.d.ts'],
+      dest: 'build/',
+      options: {
+        basePath: 'build/typescript-src/',
+        ignoreError: false,
+        noImplicitAny: true,
+        sourceMap: true
+      }
+    };
+  }
+
+  // Copy all source that is not typescript to the module's build directory.
+  export function copyModule(name:string) {
+    return {
+      expand: true, cwd: 'src/',
+      src: [name + '/**', '!**/*.ts', '!**/*.sass'],
+      dest: 'build',
+      onlyIf: 'modified'
+    };
+  }
+
+  // Samples get all compiled code (exlcuding code from sample dir itself - no
+  // recursive copying please!) in a 'lib' subdirectory.
+  export function copySampleFiles(samplePath:string, libDir:string) {
+    return {
+      files: [
+        { // Copy the sample source to the build sample directory
+          expand: true, cwd: 'src/',
+          src: [samplePath + '/**/*',
+                '!' + samplePath + '/**/*.ts',
+                '!' + samplePath + '/**/*.sass'],
+          dest: 'build/',
+          onlyIf: 'modified',
+        }, {  // Copy all modules in the build directory to the sample
+          expand: true, cwd: 'build',
+          src: ['**/*',
+                '!**/samples/**',
+                '!**/typescript-src/**'],
+          dest: 'build/' + samplePath + '/' + libDir,
+          onlyIf: 'modified'
+        }
+      ]
+    };
+  }
+
+  // Function to make jasmine spec assuming expected dir layout.
+  export function jasmineSpec(name:string) {
+    var jasmine_helpers = [
+        // Help Jasmine's PhantomJS understand promises.
+        'node_modules/es6-promise/dist/promise-*.js',
+        '!node_modules/es6-promise/dist/promise-*amd.js',
+        '!node_modules/es6-promise/dist/promise-*.min.js'
+      ];
+    return {
+      src: jasmine_helpers.concat([
+        'build/' + name + '/**/*.js',
+        '!build/' + name + '/**/*.spec.js'
+      ]),
+      options: {
+        specs: 'build/' + name + '/**/*.spec.js',
+        outfile: 'build/' + name + '/_SpecRunner.html',
+        keepRunner: true
+      }
+    };
+  }
+
+}  // module Rules
