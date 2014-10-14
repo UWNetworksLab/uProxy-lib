2ca4dc22597b8b0c39e9368551b6e5a69b6827af
Lally Singh <lally@google.com>
2014-10-14 17:31:23 -0400

    Request.js deps.

--- third_party/typings/tsd.d.ts
+++ third_party/typings/tsd.d.ts
@@ -3,3 +3,6 @@
 /// <reference path="webcrypto/WebCrypto.d.ts" />
 /// <reference path="webrtc/MediaStream.d.ts" />
 /// <reference path="webrtc/RTCPeerConnection.d.ts" />
+/// <reference path="form-data/form-data.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="request/request.d.ts" />
