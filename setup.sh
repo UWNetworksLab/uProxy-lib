f63c7d3277ecdf563f97216b23640204f021a4fb
Trevor Johnston <trevj@google.com>
2015-07-14 12:04:26 -0700

    add bower dependency, so that it does not have to be installed globally

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@
 # allows script to be run from different directories but always act on the
 # directory it is within.
 ROOT_DIR="$(cd "$(dirname $0)"; pwd)";
+NPM_BIN_DIR="$ROOT_DIR/node_modules/.bin"
 
 # A simple bash script to run commands to setup and install all dev dependencies
 # (including non-npm ones)
@@ -13,7 +14,7 @@ function runAndAssertCmd ()
     echo
     # We use set -e to make sure this will fail if the command returns an error
     # code.
-    set -e && eval $1
+    set -e && cd $ROOT_DIR && eval $1
 }
 
 # Just run the command, ignore errors (e.g. cp fails if a file already exists
@@ -22,25 +23,23 @@ function runCmd ()
 {
     echo "Running: $1"
     echo
-    eval $1
+    cd $ROOT_DIR && eval $1
 }
 
 function buildTools ()
 {
-  runCmd "cd $ROOT_DIR"
   runCmd "mkdir -p build/dev/uproxy-lib/build-tools/"
-  runCmd "cp $ROOT_DIR/src/build-tools/*.ts build/dev/uproxy-lib/build-tools/"
-  runAndAssertCmd "./node_modules/.bin/tsc --module commonjs --noImplicitAny ./build/dev/uproxy-lib/build-tools/*.ts"
+  runCmd "cp src/build-tools/*.ts build/dev/uproxy-lib/build-tools/"
+  runAndAssertCmd "$NPM_BIN_DIR/tsc --module commonjs --noImplicitAny ./build/dev/uproxy-lib/build-tools/*.ts"
   runCmd "mkdir -p ./build/tools/"
-  runCmd "cp ./build/dev/uproxy-lib/build-tools/*.js ./build/tools/"
+  runCmd "cp build/dev/uproxy-lib/build-tools/*.js build/tools/"
 }
 
 function thirdParty ()
 {
-  runCmd "cd $ROOT_DIR"
-  runAndAssertCmd "bower install --allow-root --config.interactive=false"
+  runAndAssertCmd "$NPM_BIN_DIR/bower install --allow-root --config.interactive=false"
   runAndAssertCmd "mkdir -p build/third_party"
-  runAndAssertCmd "node_modules/.bin/tsd reinstall --config ./third_party/tsd.json"
+  runAndAssertCmd "$NPM_BIN_DIR/tsd reinstall --config ./third_party/tsd.json"
   runAndAssertCmd "cp -r third_party/* build/third_party/"
   runAndAssertCmd "mkdir -p build/third_party/freedom-pgp-e2e"
   runAndAssertCmd "cp -r node_modules/freedom-pgp-e2e/dist build/third_party/freedom-pgp-e2e/"
@@ -53,7 +52,6 @@ function clean ()
 
 function installDevDependencies ()
 {
-  runCmd "cd $ROOT_DIR"
   runAndAssertCmd "npm install"
   thirdParty
   buildTools
