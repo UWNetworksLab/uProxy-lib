e4d45cf9e59b4256527ed0020434346faa55cf3f
Kenny Song <kenny.ysong@gmail.com>
2015-07-21 17:53:02 -0400

    Code review changes r3

--- third_party/freedom-typings/port-control.d.ts
+++ third_party/freedom-typings/port-control.d.ts
@@ -3,45 +3,45 @@
 declare module freedom_PortControl {
     // Object representing a port mapping
     interface Mapping {
-        internalIp: string;
-        internalPort: number;
-        externalIp?: string;
-        externalPort: number;
-        lifetime: number;
-        protocol: string;
-        timeoutId?: number;
-        nonce?: number[];
+        internalIp :string;
+        internalPort :number;
+        externalIp ?:string;
+        externalPort :number;
+        lifetime :number;
+        protocol :string;
+        timeoutId ?:number;
+        nonce ?:number[];
     }
     // A collection of Mappings
     interface ActiveMappings {
-        [extPort: string]: Mapping;
+        [extPort :string] :Mapping;
     }
     // An object returned by probeProtocolSupport()
     interface ProtocolSupport {
-        natPmp: boolean;
-        pcp: boolean;
-        upnp: boolean;
+        natPmp :boolean;
+        pcp :boolean;
+        upnp :boolean;
     }
     // Main interface for the module
     interface PortControl {
-        addMapping(intPort: number, extPort: number, lifetime: number): Promise<Mapping>;
-        deleteMapping(extPort: number): Promise<boolean>;
-        probeProtocolSupport(): Promise<ProtocolSupport>;
+        addMapping(intPort:number, extPort:number, lifetime:number) :Promise<Mapping>;
+        deleteMapping(extPort:number) :Promise<boolean>;
+        probeProtocolSupport() :Promise<ProtocolSupport>;
 
-        probePmpSupport(): Promise<boolean>;
-        addMappingPmp(intPort: number, extPort: number, lifetime: number): Promise<Mapping>;
-        deleteMappingPmp(extPort: number): Promise<boolean>;
+        probePmpSupport() :Promise<boolean>;
+        addMappingPmp(intPort:number, extPort:number, lifetime:number) :Promise<Mapping>;
+        deleteMappingPmp(extPort:number) :Promise<boolean>;
 
-        probePcpSupport(): Promise<boolean>;
-        addMappingPcp(intPort: number, extPort: number, lifetime: number): Promise<Mapping>;
-        deleteMappingPcp(extPort: number): Promise<boolean>;
+        probePcpSupport() :Promise<boolean>;
+        addMappingPcp(intPort:number, extPort:number, lifetime:number) :Promise<Mapping>;
+        deleteMappingPcp(extPort:number) :Promise<boolean>;
 
-        probeUpnpSupport(): Promise<boolean>;
-        addMappingUpnp(intPort: number, extPort: number, lifetime: number): Promise<Mapping>;
-        deleteMappingUpnp(extPort: number): Promise<boolean>;
+        probeUpnpSupport() :Promise<boolean>;
+        addMappingUpnp(intPort:number, extPort:number, lifetime:number) :Promise<Mapping>;
+        deleteMappingUpnp(extPort:number) :Promise<boolean>;
 
-        getActiveMappings(): Promise<ActiveMappings>;
-        getPrivateIps(): Promise<string[]>;
-        close(): Promise<void>;
+        getActiveMappings() :Promise<ActiveMappings>;
+        getPrivateIps() :Promise<string[]>;
+        close() :Promise<void>;
     }
 }
