137affc9e6cc5cb127832203d59732dd9120d61a
Brandon Wiley <bwiley@google.com>
2015-07-24 11:46:40 -0400

    Added new transformers for functional testing of packet fragmentation

---
+++ src/fancy-transformers/packetLengthExtender.ts
@@ -0,0 +1,40 @@
+
+// TODO(bwiley): update uTransformers to be compatible with require
+// TODO(ldixon): update to a require-style inclusion.
+// e.g.
+//  import Transformer = require('uproxy-obfuscators/transformer');
+/// <reference path='../../../third_party/uTransformers/utransformers.d.ts' />
+
+import logging = require('../logging/logging');
+import PacketLengthShaper = require('./packetLengthShaper');
+
+var log :logging.Log = new logging.Log('fancy-transformers');
+
+// TODO(bwiley): Convert /* */ to // as specified in the style guide
+
+/**
+ * An obfuscator that only modifies packet length.
+ * To start out, this is a very simple (and bad) packet length obfuscator.
+ * It follows this logic:
+ * Case 1 - buffer length + 2 == target: return length(buffer) + buffer
+ * Case 2 - buffer length + 2 > target:  return length(target) + randomBytes(target)
+ * Case 3 - buffer length + 2 < target:  return length(target) + buffer + randomBytes(target)
+ */
+class PacketLengthExtender extends PacketLengthShaper implements Transformer {
+  public constructor() {
+    super();
+    log.info('Constructed packet length extender');
+  }
+
+  /** Get the target minimum and maximum lengths. */
+  public configure = (json:string) : void => {
+    this.superConfigure(json);
+  }
+
+  public transform = (buffer:ArrayBuffer) : ArrayBuffer[] => {
+    log.info('Transforming');
+    return this.shapePacketLength(buffer, buffer.byteLength+37);
+  }
+}
+
+export = PacketLengthExtender;
---
+++ src/fancy-transformers/packetLengthShortener.ts
@@ -0,0 +1,42 @@
+
+// TODO(bwiley): update uTransformers to be compatible with require
+// TODO(ldixon): update to a require-style inclusion.
+// e.g.
+//  import Transformer = require('uproxy-obfuscators/transformer');
+/// <reference path='../../../third_party/uTransformers/utransformers.d.ts' />
+
+import logging = require('../logging/logging');
+import PacketLengthShaper = require('./packetLengthShaper');
+
+var log :logging.Log = new logging.Log('fancy-transformers');
+
+// TODO(bwiley): Convert /* */ to // as specified in the style guide
+
+/**
+ * An obfuscator that only modifies packet length.
+ * To start out, this is a very simple (and bad) packet length obfuscator.
+ * It follows this logic:
+ * Case 1 - buffer length + 2 == target: return length(buffer) + buffer
+ * Case 2 - buffer length + 2 > target:  return length(target) + randomBytes(target)
+ * Case 3 - buffer length + 2 < target:  return length(target) + buffer + randomBytes(target)
+ */
+class PacketLengthShortener extends PacketLengthShaper implements Transformer {
+  public constructor() {
+    super();
+    log.info('Constructed packet length shortener');
+  }
+
+  /** Get the target minimum and maximum lengths. */
+  public configure = (json:string) : void => {
+    this.superConfigure(json);
+  }
+
+  public transform = (buffer:ArrayBuffer) : ArrayBuffer[] => {
+//    log.info('Transforming');
+    var results=this.shapePacketLength(buffer, buffer.byteLength+35);
+//    log.info('Sending %1 packets', results.length);
+    return results;
+  }
+}
+
+export = PacketLengthShortener;
