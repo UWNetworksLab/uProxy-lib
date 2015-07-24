abbbe026f82c2d61bdf7ca2f866068a937fc9a5c
Brandon Wiley <bwiley@google.com>
2015-07-24 11:44:52 -0400

    Migrated some methods from the packet shaper and defragmenter into fragment

--- src/fancy-transformers/fragment.ts
+++ src/fancy-transformers/fragment.ts
@@ -50,17 +50,33 @@ var split_ = (buffer:ArrayBuffer, firstLen:number) : Array<ArrayBuffer> => {
   return [first.buffer, last.buffer];
 }
 
+/* Takes a two byte (network byte order) representation of a number and returns
+ * the number.
+ */
+ // TODO(bwiley): Byte order may be backward
+ // TODO(bwiley): Fix type error
+var decodeLength_ = (buffer:ArrayBuffer) : number => {
+  var bytes = new Uint8Array(buffer);
+  var result = (bytes[0] << 8) | bytes[1];
+  return result;
+}
+
+
 class Fragment {
+  public length : number;
   public id : ArrayBuffer;
   public index : number;
   public count : number;
   public payload : ArrayBuffer;
+  public padding : ArrayBuffer;
 
-  public constructor(id:ArrayBuffer, index:number, count:number, payload:ArrayBuffer) {
+  public constructor(length:number, id:ArrayBuffer, index:number, count:number, payload:ArrayBuffer, padding:ArrayBuffer) {
+    this.length=length;
     this.id=id;
     this.index=index;
     this.count=count;
     this.payload=payload;
+    this.padding=padding;
   }
 
   static randomId = () : ArrayBuffer => {
@@ -71,9 +87,14 @@ class Fragment {
     return bytes.buffer;
   }
 
-  static decodeFragment = (buffer:ArrayBuffer, length:number) : Fragment => {
+  static decodeFragment = (buffer:ArrayBuffer) : Fragment => {
 //    log.debug('Decode fragment %1 %2', buffer.byteLength, length);
-    var parts=split_(buffer, 32);
+    var parts = split_(buffer, 2);
+    var lengthBytes = parts[0];
+    var length = decodeLength_(lengthBytes);
+    buffer = parts[1];
+
+    parts=split_(buffer, 32);
     var fragmentId=parts[0];
     buffer=parts[1];
 
@@ -86,26 +107,40 @@ class Fragment {
 //    log.debug('Decoded fragment %1 %2 %3', fragmentId, fragmentNumber, totalNumber);
 
     var payload : ArrayBuffer=null;
+    var padding : ArrayBuffer=null;
 
     if(buffer.byteLength > length) {
       parts=split_(buffer, length);
       payload=parts[0];
+      padding=parts[1];
 //      log.debug('shortened payoad %1 %2 %3', buffer.byteLength, length, payload.byteLength);
     } else if (buffer.byteLength == length) {
       payload=buffer;
+      padding=new ArrayBuffer(0);
 //      log.debug('perect payoad %1 %2 %3', buffer.byteLength, length, payload.byteLength);
     } else { // buffer.byteLength < length
       throw new Error("Short buffer");
     }
 
-    var fragment=new Fragment(fragmentId, fragmentNumber, totalNumber, payload);
+    var fragment=new Fragment(length, fragmentId, fragmentNumber, totalNumber, payload, padding);
 //    log.info("Decoding %1 %2 %3", fragment.id, fragment.index, fragment.count);
     return fragment;
   }
 
   public encodeFragment = () : ArrayBuffer => {
 //    log.info("Encoding %1 %2 %3", this.id, this.index, this.count);
-    return this.assemble_([this.id, encodeByte_(this.index), encodeByte_(this.count), this.payload]);
+    return this.assemble_([this.encodeLength_(this.length), this.id, encodeByte_(this.index), encodeByte_(this.count), this.payload, this.padding]);
+  }
+
+  /* Takes a number and returns a two byte (network byte order) representation
+   * of this number.
+   */
+   // TODO(bwiley): Byte order may be backward
+  private encodeLength_ = (len:number) : ArrayBuffer => {
+    var bytes = new Uint8Array(2);
+    bytes[0] = Math.floor(len >> 8);
+    bytes[1] = Math.floor((len << 8) >> 8);
+    return bytes.buffer;
   }
 
   private assemble_ = (buffers:ArrayBuffer[]) : ArrayBuffer => {
@@ -125,6 +160,17 @@ class Fragment {
     }
 
     return result.buffer;
+  }
+
+  /* Takes a two byte (network byte order) representation of a number and returns
+   * the number.
+   */
+   // TODO(bwiley): Byte order may be backward
+   // TODO(bwiley): Fix type error
+  private decodeLength_ = (buffer:ArrayBuffer) : number => {
+    var bytes = new Uint8Array(buffer);
+    var result = (bytes[0] << 8) | bytes[1];
+    return result;
   }
 }
 
