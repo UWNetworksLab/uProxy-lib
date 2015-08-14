b38301a2d4018debf3d8b92efda9c18676fb2c28
Kenny Song <kenny.ysong@gmail.com>
2015-08-14 11:11:41 -0400

    Port control parameter added to all relevant class constructors and methods

--- third_party/freedom-typings/port-control.d.ts
+++ third_party/freedom-typings/port-control.d.ts
@@ -37,7 +37,8 @@ declare module freedom_PortControl {
         deleteMappingPcp(extPort:number) :Promise<boolean>;
 
         probeUpnpSupport() :Promise<boolean>;
-        addMappingUpnp(intPort:number, extPort:number, lifetime:number) :Promise<Mapping>;
+        addMappingUpnp(intPort:number, extPort:number, lifetime:number, 
+                       controlUrl?:string) :Promise<Mapping>;
         deleteMappingUpnp(extPort:number) :Promise<boolean>;
 
         getActiveMappings() :Promise<ActiveMappings>;
