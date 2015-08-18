4c5d987793c0ce72774a84680aae73c834d4c2a0
Brandon Wiley <bwiley@google.com>
2015-08-18 16:49:41 -0400

    Added documentation about possible return values.

--- third_party/uTransformers/utransformers.d.ts
+++ third_party/uTransformers/utransformers.d.ts
@@ -21,7 +21,11 @@ interface Transformer {
    * Transforms a piece of data to obfuscated form.
    *
    * @param {ArrayBuffer} plaintext data that needs to be obfuscated.
-   * @return {ArrayBuffer[]} array of obfuscated data, empty if failed.
+   * @return {ArrayBuffer[]} list of ArrayBuffers of obfuscated data.
+   * The list can contain zero, one, or more than one items.
+   * In the case of fragmentation:
+   *   When fragmention occurs, the list will have more than one item.
+   *   When there is no fragmentation, the list will have one item.
    */
   transform(buffer:ArrayBuffer) : ArrayBuffer[];
 
@@ -29,7 +33,11 @@ interface Transformer {
    * Restores data from obfuscated form to original form.
    *
    * @param {ArrayBuffer} ciphertext obfuscated data.
-   * @return {ArrayBuffer} array of original data, empty if failed.
+   * @return {ArrayBuffer} list of ArrayBuffers of original data.
+   * The list can contain zero, one, or more than one items.
+   * In the case of fragmentation:
+   *   When receiving a fragment, the list will have zero items,
+   *   unless it was the last fragment, then the list will have one item.
    */
   restore(buffer:ArrayBuffer) : ArrayBuffer[];
 
