c6e9d8c5d4bcf3da86ea9e4b82f3098bfaf758a1
Trevor Johnston <trevj@google.com>
2016-06-14 15:28:56 -0400

    remove code, update README to point to uproxy

--- src/cloud/digitalocean/freedom-module.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path='../../../../third_party/typings/browser.d.ts' />
-
-import Provisioner = require('./provisioner');
-
-declare const freedom: freedom.FreedomInModuleEnv;
-
-freedom().providePromises(Provisioner);
-
-export = Provisioner
